92e4a4a89d4725bb18c4f309dc0112419d797408
Marco Paredes <marcop_93_04@hotmail.com>
2017-10-22 14:26:02 -0300

    Advanced Lists

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,7 @@ import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
 import { NativeScriptUISideDrawerModule } from "nativescript-telerik-ui/sidedrawer/angular";
+import { NativeScriptUIListViewModule } from "nativescript-telerik-ui/listview/angular";
 import { TNSFontIconModule } from 'nativescript-ngx-fonticon';
 
 import { MenuComponent } from './menu/menu.component';
@@ -12,6 +13,7 @@ import { DrawerComponent } from "./shared/drawer/drawer.component";
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 import { DishService } from './services/dish.service';
@@ -38,7 +40,8 @@ import { baseURL } from './shared/baseurl';
         NativeScriptUISideDrawerModule,
         TNSFontIconModule.forRoot({
             'fa': './fonts/font-awesome.min.css'
-        })
+        }),
+        NativeScriptUIListViewModule
     ],
     declarations: [
         AppComponent,
@@ -47,7 +50,8 @@ import { baseURL } from './shared/baseurl';
         DrawerComponent,
         HomeComponent,
         ContactComponent,
-        AboutComponent
+        AboutComponent,
+        FavoritesComponent
     ],
     providers: [
         DishService,
